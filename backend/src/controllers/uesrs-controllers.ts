6ba220c5814a774a56e72d8ff70a8ce9234f4bae
nasir-buddy <syedali172000@gmail.com>
2024-03-28 01:16:01 +0530

    chatbot: 'set cookie for login and signup complete'

--- backend/src/controllers/uesrs-controllers.ts
+++ backend/src/controllers/uesrs-controllers.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js"
 import { hash, compare } from 'bcrypt'
 import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 export const getALLUsers = async (req: Request, res: Response, next: NextFunction) => {
     // get all users from database
     try {
@@ -23,6 +24,27 @@ export const userSignup = async (req: Request, res: Response, next: NextFunction
         const hashedPassword = await hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
         await user.save();
+
+        // create token and store cookie 
+        // clearing the cookie 
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: 'localhost',
+            signed: true,
+            path: "/",
+        });
+        // token will create after sussessfull login
+        const token = createToken(user._id.toString(), user.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, { 
+            path: '/',
+            domain: 'localhost', 
+            expires, 
+            httpOnly: true , 
+            signed: true 
+        });
+
         return res.status(200).json({ message: "OK", id: user._id.toString() });
     } catch (error) {
         console.log(error);
@@ -42,11 +64,19 @@ export const userLogin = async (req: Request, res: Response, next: NextFunction)
         if (!isPasswordMatch) {
             return res.status(403).json({ message: "Incorrect Password" });
         }
+        // clearing the cookie 
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: 'localhost',
+            signed: true,
+            path: "/",
+        });
         // token will create after sussessfull login
         const token = createToken(user._id.toString(), user.email, "7d");
         const expires = new Date();
         expires.setDate(expires.getDate() + 7);
-        res.cookie("auth.token", token, { path: '/',
+        res.cookie(COOKIE_NAME, token, { 
+            path: '/',
             domain: 'localhost', 
             expires, 
             httpOnly: true , 
