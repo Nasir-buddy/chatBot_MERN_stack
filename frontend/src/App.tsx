7922aef9a62688e97ea202ce37e240fef15b0065
nasir-buddy <syedali172000@gmail.com>
2024-03-28 01:50:45 +0530

    chatbot: 'set all folders and necessary files and routing too'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
-
+import Header from "./components/Header"
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import NotFount from "./pages/NotFount"
+import Chat from "./pages/Chat"
 function App() {
 
   return (
-    <main>Hello world</main>
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/chat" element={<Chat />}></Route>
+        <Route path="*" element={<NotFount />}></Route>
+      </Routes>
+    </main>
   )
 }
 
