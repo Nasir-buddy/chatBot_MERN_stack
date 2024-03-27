eab658107f6fd260d744a4d4d5c2638e82415276
nasir-buddy <syedali172000@gmail.com>
2024-03-27 21:34:08 +0530

    chatbot: 'set user signup test'

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { getALLUsers } from '../controllers/uesrs-controllers.js';
+import { getALLUsers, userSignup } from '../controllers/uesrs-controllers.js';
 
 const userRoutes = Router();
 
 userRoutes.get('/', getALLUsers);
-
+userRoutes.post('/signup', userSignup);
 export default userRoutes;
