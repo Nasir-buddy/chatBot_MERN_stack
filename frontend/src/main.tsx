691bb72603818e3512c84250c3ebc98c2931d4c3
nasir-buddy <syedali172000@gmail.com>
2024-03-29 00:27:20 +0530

    chatbot: 'implement login method'

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,30 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { createTheme, ThemeProvider } from '@mui/material'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.tsx'
+import axios from 'axios'
+import { Toaster } from 'react-hot-toast'
+
+axios.defaults.baseURL = "https://localhost:5000/api/v1";
+axios.defaults.withCredentials = true;
 const theme = createTheme({
   typography: {
     fontFamily: "Roboto Slab, serif",
     allVariants: { color: "White" }
   }
 })
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>
+  <AuthProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <Toaster position='top-right'/>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </AuthProvider>
 
 )
