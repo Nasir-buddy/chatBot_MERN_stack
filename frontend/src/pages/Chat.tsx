20a2dff97bead1bd0857ee62b0352ec6a5d20e50
nasir-buddy <syedali172000@gmail.com>
2024-03-29 12:36:21 +0530

    chatbot: 'implemented chat section'

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,8 +1,102 @@
+import { Avatar, Box, Button, Typography } from '@mui/material'
 import React from 'react'
-
+import { useAuth } from '../context/AuthContext'
+import { red } from '@mui/material/colors';
+import ChatItem from '../components/chat/ChatItem';
+const chatMessage = [
+    { "role": "User", "content": "Hi there, how can you assist me today?" },
+    { "role": "assistant", "content": "Hello! I'm here to help. What do you need assistance with?" },
+    { "role": "User", "content": "I need help with setting up my email." },
+    { "role": "assistant", "content": "Sure thing! Could you please provide me with the email service provider you're using?" },
+    { "role": "User", "content": "I'm using Gmail." },
+    { "role": "assistant", "content": "Great! I'll guide you through the setup process step by step." },
+    { "role": "User", "content": "Thank you!" },
+    { "role": "assistant", "content": "You're welcome. Let me know if there's anything else I can assist you with." }
+]
 const Chat = () => {
+  const auth = useAuth();
   return (
-    <div>Chat</div>
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        mt: 3,
+        gap: 3,
+      }}
+    >
+      <Box sx={{ display: { md: 'flex', xs: 'none', sm: 'none' }, flex: 0.2, flexDirection: 'column  ' }}>
+        <Box sx={{ display: 'flex', width: '100%', height: '60vh', bgcolor: 'rgb(17,29,39)', borderRadius: 5, flexDirection: 'column', mx: 3 }}>
+          <Avatar sx={{ mx: 'auto', my: 2, bgcolor: 'white', color: 'black', fontWeight: 700 }}>
+            {auth?.user?.name[0]}
+            {auth?.user?.name.split('')[1][0]}
+          </Avatar>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
+            You are talking to a ChatBOT
+          </Typography>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans", my: 4, p: 3 }}>
+            You can ask some questions related to Knowledge, Business, Advices,
+            Education, etc. But avoid sharing personal information
+          </Typography>
+          <Button
+            // onClick={handleDeleteChats}
+            sx={{
+              width: "200px",
+              my: "auto",
+              color: "white",
+              fontWeight: "700",
+              borderRadius: 3,
+              mx: "auto",
+              bgcolor: red[300],
+              ":hover": {
+                bgcolor: red.A400,
+              },
+            }}
+          >
+            Clear Conversation
+          </Button>
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          flex: { md: 0.8, xs: 1, sm: 1 },
+          flexDirection: "column",
+          px: 3,
+        }}>
+        <Typography
+          sx={{
+            fontSize: "40px",
+            color: "white",
+            mb: 2,
+            mx: "auto",
+            fontWeight: "600",
+          }}
+        >
+          Model - GPT 3.5 Turbo
+        </Typography>
+        <Box
+          sx={{
+            width: "100%",
+            height: "60vh",
+            borderRadius: 3,
+            mx: "auto",
+            display: "flex",
+            flexDirection: "column",
+            overflow: "scroll",
+            overflowX: "hidden",
+            overflowY: "auto",
+            scrollBehavior: "smooth",
+          }}
+        >
+          {chatMessage.map((chat, index)=>(
+           <ChatItem content={chat.content} role={chat.role} key={index} />
+          ))}
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
