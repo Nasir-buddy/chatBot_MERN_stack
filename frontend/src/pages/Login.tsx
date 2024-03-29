c5b3aa54c1a1997c38fe58e1bf4983861b4c1fd5
nasir-buddy <syedali172000@gmail.com>
2024-03-29 20:06:15 +0530

    chatbot: 'protected route for login page'

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,12 +1,15 @@
 import { Box, Typography, Button } from '@mui/material'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import CustomizedInput from '../components/shared/CustomizedInput'
 import { TbLogin2 } from "react-icons/tb";
 import { toast } from 'react-hot-toast';
 import { useAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
+  const navigate = useNavigate();
   const auth = useAuth();
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -22,6 +25,13 @@ const Login = () => {
       toast.error("Signing In Failed!!", { id: "login" });
     }
   }
+
+  useEffect(() => {
+    if(auth?.user){
+      return navigate("/chat");
+    }
+  }, [auth])
+
   return (
     <Box width={'100%'} height={'100%'} display={'flex'} flex={'1'}>
       <Box padding={8} mt={8} display={{ md: 'flex', sm: 'none', xs: 'none' }}>
