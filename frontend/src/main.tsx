c0ce58d69f176c4e998351b594d6cd19104dc215
nasir-buddy <syedali172000@gmail.com>
2024-03-30 14:44:52 +0530

    chatbot: 'created seprate env file'

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from './context/AuthContext.tsx'
 import { Toaster } from 'react-hot-toast'
 import axios from 'axios'
 
-axios.defaults.baseURL = "https://chat-bot-mern-stack.vercel.app/";
+axios.defaults.baseURL = "http://localhost:5000/api/v1";
 axios.defaults.withCredentials = true;
 const theme = createTheme({
   typography: {
@@ -19,14 +19,14 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
-  <AuthProvider>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <Toaster position='top-right' />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </AuthProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <Toaster position='top-right' />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthProvider>
 
 
 )
